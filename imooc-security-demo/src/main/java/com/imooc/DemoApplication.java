7996bdeddfa2fee23c0893d91c7ea44f0104a50f
hornyun <hornyun@sina.com>
2018-06-04 18:21:47 +0800

    Hello Spring Security

---
+++ imooc-security-demo/src/main/java/com/imooc/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhy
+ * @version 1.0
+ * @classDesc:
+ * @date 2018/6/4
+ * @copyright: 上海英和
+ * @QQ: 583760722
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello spring security";
+    }
+}
